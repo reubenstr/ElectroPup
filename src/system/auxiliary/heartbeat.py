9886ce41e6681f106489d48728696672b5e6069d
reubenstr <reubenstrangelove@gmail.com>
2025-08-01 16:48:31 -0400

    notes update

--- src/system/auxiliary/heartbeat.py
+++ src/system/auxiliary/heartbeat.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python3
+import time
+import RPi.GPIO as GPIO
 
 """
 
 Generates a heartbeat signal (square wave) that allows the auxiliary board 
-to determine if the Raspberry Pi is booted and operational.
+to determine if the Raspberry Pi is booted.
 
 Use the install_heartbeat.sh script to install this script as a service.
 
 """
 
-import time
-import RPi.GPIO as GPIO
-
 pin_number = 17
 rate = 0.025
 
 try:
-    print(f"[Heartbeat] starting heartbeat")
+    print(f"[Heartbeat] starting")
     
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(pin_number, GPIO.OUT)
